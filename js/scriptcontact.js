190eeb5a9635a05fdf173cb98bc006306eb4b74a
man2025git <kanok2023tanya@gmail.com>
2025-04-23 14:16:21 +0700

    layout grid vertion1

--- js/scriptcontact.js
+++ js/scriptcontact.js
@@ -6,13 +6,15 @@ $(function () {
             $(".menuopen").removeClass("active");
             $(".menuclose").addClass("active");
             $(".navbarmenu ul").addClass("active");
-            $(".home").addClass("active");
+            // $(".about_container").addClass("active");
+            $(".header_menu").addClass("active");
           } 
           else {
             $(".menuopen").addClass("active");
             $(".menuclose").removeClass("active");
             $(".navbarmenu ul").removeClass("active");
-            $(".home").removeClass("active");
+            // $(".about_container").removeClass("active");
+            $(".header_menu").removeClass("active");
         }
     });
 });
